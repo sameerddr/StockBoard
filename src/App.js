651ae32310d5ddcf529f7d0206cec97bf6d7f383
karthik kjs <104209441+karthikgeek1@users.noreply.github.com>
2022-11-11 15:56:44 +0530

    created 2 static cards

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "antd/dist/antd.css";
 import "./App.css";
+import Cards from "./Components/Cards";
 import StockLayout from "./Components/StockLayout";
 function App() {
   return (
     <div className="App">
       <StockLayout />
+      {/* <Cards/> */}
     </div>
   );
 }
