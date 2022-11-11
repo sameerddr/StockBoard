8fc8ded33ec45432bbee2df28b9bbdac7e35006d
karthik kjs <104209441+karthikgeek1@users.noreply.github.com>
2022-11-11 23:07:03 +0530

    cards

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -15,7 +15,7 @@ const Cards = () => {
                 extra={<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8q0metut-4bQutjD3LQhql0bfSaXE_4ETJQ&usqp=CAU'
                     style={{ height: "50px", width: "50px", borderRadius: '50%' }} />}
             >
-                <h3 style={{ color: "white", textAlign: "left" }}>price:<span style={{ color: "green", paddingLeft:"10px" }}>$296.00</span></h3>
+                <h3>price:<span>$296.00</span></h3>
             </Card>
             <Card
                 title="AMD"
@@ -26,7 +26,7 @@ const Cards = () => {
                 extra={<img src='https://pbs.twimg.com/profile_images/1145524454170062848/U4lxVYEw_400x400.png'
                     style={{ height: "50px", width: "50px", borderRadius: '50%' }} />}
             >
-                <h3 >price:<span style={{ color: "green", paddingLeft:"10px" }}> $64.32</span></h3>
+                <h3 >price:<span> $64.32</span></h3>
             </Card>
         {/* </div> */}
         </>
