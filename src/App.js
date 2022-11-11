b1e51c62bb0ef042258af8c46594d8c79b07d34b
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-11 14:29:51 +0530

    layout Completed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "antd/dist/antd.css";
 import "./App.css";
-import StockLayout from "./Components/StockLayout";
+import StockLayout from "./Components/Layout/StockLayout";
 function App() {
   return (
     <div className="App">
