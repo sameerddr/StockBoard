ab0d90e720fe865b8d14fc74fe518069809564fb
Sameer Varshney <105305958+sameerddr@users.noreply.github.com>
2022-11-18 12:39:28 +0530

    graph added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import "antd/dist/antd.css";
-
+import Graph1 from "./Components/Graphs/Graph1";
 import StockLayout from "./Components/Layout/StockLayout";
 import "./App.css";
-
 
 function App() {
   return (
